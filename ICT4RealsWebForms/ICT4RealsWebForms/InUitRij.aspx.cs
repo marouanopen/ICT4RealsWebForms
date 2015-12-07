8a24cbabe13e04d7f010da90ce76cd8601bb9099
Stefan Verhoeve <S.verhoeve@student.fontys.nl>
2015-12-07 14:21:06 +0100

    inrijdende trams

--- ICT4RealsWebForms/ICT4RealsWebForms/InUitRij.aspx.cs
+++ ICT4RealsWebForms/ICT4RealsWebForms/InUitRij.aspx.cs
@@ -20,10 +20,19 @@ namespace ICT4RealsWebForms
         private Repairservice rpService = new Repairservice(1, "repair", DateTime.Today, DateTime.Today, 1, 1);
         protected void Page_Load(object sender, EventArgs e)
         {
-            ddlStatus.Items.Add("Ok");
-            ddlStatus.Items.Add("Vies");
-            ddlStatus.Items.Add("Defect");
-            ddlStatus.Items.Add("Vies en Defect");
+            if(!IsPostBack)
+            {
+                ddlStatus.Items.Clear();
+                ddlStatus.Items.Add("Ok");
+                ddlStatus.Items.Add("Vies");
+                ddlStatus.Items.Add("Defect");
+                ddlStatus.Items.Add("Vies en Defect");
+            }
+                administration = Login.administration; 
+            //this.parkingsystem = new Parkingsystem();
+            //this.padatabase = new PAdatabase();
+            
+            remiseRefresh();//not sure
         }
         public void btnIncomingTram_Click(object sender, EventArgs e)
         {
@@ -64,7 +73,7 @@ namespace ICT4RealsWebForms
                         else
                         {
                             rail = parkingsystem.InsertTramNr(Convert.ToInt32(tbTramnr.Text), status);
-                            tram._Status = status;
+                            //tram._Status = status;
                             if (status == 2)
                             {
                                 soort = "Schoonmaak";
--- ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
+++ ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
@@ -4,13 +4,69 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using UserInterface_Mockup_ICT4Reals.AdminSystem;
 
 namespace ICT4RealsWebForms
 {
     public partial class Login : System.Web.UI.Page
     {
+        private string username;
+        private string password;
+        public static Administration administration;
         protected void Page_Load(object sender, EventArgs e)
         {
+            administration = new Administration();
+        }
+
+        /// <summary>
+        /// constructor
+        /// </summary>
+        public Login()
+        {
+            
+        }
+        /// <summary>
+        /// occurs when "btnlogin"is clicked
+        /// </summary>
+        /// <param name="sender">the control that was clicked</param>
+        /// <param name="e"></param>
+        public void btlogin_Click(object sender, EventArgs e)
+        {
+            username = (string)tbUserName.Text;
+            password = (string)tbPassword.Text;
+
+            //pseudo doquery met check of hij lines teruggeeft wanneer je naar password vraagt die bij ingevulde gebruikersnaam hoort
+            //if so login met gebruiker met de data van die gebruiker uit de database //check in de administrationklasse
+            //if (administration.LogIn(username, password))
+            if(administration.LogIn("beheerder@mail.com", "ict4reals"))
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You have succesfully logged in')", true);
+                Response.Redirect("beheer.aspx");
+            }
+            else
+            {
+                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Something went wrong when trying to login, please try again')", true);
+            }
+            if (Administration.LoggedInUser.RoleId == 1)
+            {
+
+            }
+            if (Administration.LoggedInUser.RoleId == 2)
+            {
+                ///rechten hier
+            }
+            if (Administration.LoggedInUser.RoleId == 3)
+            {
+                //rechten hier
+            }
+            if (Administration.LoggedInUser.RoleId == 4)
+            {
+                //rechten hier
+            }
+            if (Administration.LoggedInUser.RoleId == 5)
+            {
+                ///rechten hier
+            }
 
         }
     }
