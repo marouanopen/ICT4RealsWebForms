9bdb415f2dbce352b9a9414a7c704e845b30dd0f
Stefan Verhoeve <S.verhoeve@student.fontys.nl>
2015-12-07 14:27:08 +0100

    namespace fix

--- ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
+++ ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-using UserInterface_Mockup_ICT4Reals.AdminSystem;
+using ICT4RealsWebForms.AdminSystem;
 
 namespace ICT4RealsWebForms
 {
