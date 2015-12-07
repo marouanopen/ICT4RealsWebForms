ca7cdafee0619fb1cd50913fc3a0953a67d4597f
marouanopen <marouan.bakour96@gmail.com>
2015-12-07 14:47:25 +0100

    Added nullrefrence exeption handler for login

--- ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
+++ ICT4RealsWebForms/ICT4RealsWebForms/Login.aspx.cs
@@ -34,38 +34,45 @@ namespace ICT4RealsWebForms
         {
             username = (string)tbUserName.Text;
             password = (string)tbPassword.Text;
-
-            //pseudo doquery met check of hij lines teruggeeft wanneer je naar password vraagt die bij ingevulde gebruikersnaam hoort
-            //if so login met gebruiker met de data van die gebruiker uit de database //check in de administrationklasse
-            //if (administration.LogIn(username, password))
-            if(administration.LogIn("beheerder@mail.com", "ict4reals"))
+            try
             {
-                ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You have succesfully logged in')", true);
-                Response.Redirect("beheer.aspx");
+                //pseudo doquery met check of hij lines teruggeeft wanneer je naar password vraagt die bij ingevulde gebruikersnaam hoort
+                //if so login met gebruiker met de data van die gebruiker uit de database //check in de administrationklasse
+                //if (administration.LogIn(username, password))
+                if(administration.LogIn("beheerder@mail.com", "ict4reals"))
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('You have succesfully logged in')", true);
+                    Response.Redirect("beheer.aspx");
+                }
+                else
+                {
+                    ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Something went wrong when trying to login, please try again')", true);
+                
+                }
+                if (Administration.LoggedInUser.RoleId == 1)
+                {
+
+                }
+                if (Administration.LoggedInUser.RoleId == 2)
+                {
+                    ///rechten hier
+                }
+                if (Administration.LoggedInUser.RoleId == 3)
+                {
+                    //rechten hier
+                }
+                if (Administration.LoggedInUser.RoleId == 4)
+                {
+                    //rechten hier
+                }
+                if (Administration.LoggedInUser.RoleId == 5)
+                {
+                    ///rechten hier
+                }
             }
-            else
+            catch (NullReferenceException)
             {
                 ClientScript.RegisterStartupScript(this.GetType(), "alert", "alert('Something went wrong when trying to login, please try again')", true);
-            }
-            if (Administration.LoggedInUser.RoleId == 1)
-            {
-
-            }
-            if (Administration.LoggedInUser.RoleId == 2)
-            {
-                ///rechten hier
-            }
-            if (Administration.LoggedInUser.RoleId == 3)
-            {
-                //rechten hier
-            }
-            if (Administration.LoggedInUser.RoleId == 4)
-            {
-                //rechten hier
-            }
-            if (Administration.LoggedInUser.RoleId == 5)
-            {
-                ///rechten hier
             }
 
         }
